26102e9dd06654fd149abbbcca9db49b039b754e
gere149 <gere.delatorre@gmail.com>
2025-11-14 16:08:04 +0100

    Push 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera m_Camera;
+    CharacterController m_CharacterController;
+    Animator m_Animator;
+    public float m_RunSpeed;
+    public float m_WalkSpeed;
+    float m_VerticalSpeed = 0.0f;
+    private void Awake()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+        m_Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 l_Right = m_Camera.transform.right;
+        Vector3 l_Forward = m_Camera.transform.forward;
+        Vector3 l_Movement = Vector3.zero;
+        
+        l_Right.y = 0;
+        l_Right.Normalize();
+        l_Forward.y = 0;
+        l_Forward.Normalize();
+
+        if (Input.GetKey(KeyCode.D))
+            l_Movement = l_Right;
+        else if (Input.GetKey(KeyCode.A))
+            l_Movement =- l_Right;
+
+        if (Input.GetKey(KeyCode.W))
+            l_Movement += l_Forward;
+        else if (Input.GetKey(KeyCode.S))
+            l_Movement -= l_Forward;
+
+        l_Movement.Normalize();
+        float l_SpeedAnimatorValue = 0.5f;
+        float l_Speed = m_WalkSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            l_Speed = m_RunSpeed;
+            l_SpeedAnimatorValue = 1.0f;
+        }
+        if (l_Movement.sqrMagnitude==0.0f)
+            m_Animator.SetFloat("Speed", 0.0f);
+        else
+            m_Animator.SetFloat("Speed", l_SpeedAnimatorValue);
+
+        l_Movement*=l_Speed*Time.deltaTime;
+        m_VerticalSpeed += Physics.gravity.y * Time.deltaTime;
+        l_Movement.y = m_VerticalSpeed * Time.deltaTime;
+        CollisionFlags l_CollisionFlags = m_CharacterController.Move(l_Movement);
+        if ((l_CollisionFlags & CollisionFlags.CollidedBelow) != 0)
+            m_VerticalSpeed = 0.0f;
+        else if ((l_CollisionFlags& CollisionFlags.CollidedAbove) != 0 && m_VerticalSpeed>0.0f)
+            m_VerticalSpeed=0.0f;
+    }
+}
